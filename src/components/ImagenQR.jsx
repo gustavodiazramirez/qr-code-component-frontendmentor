8383f03164819f0d12fb9468e254cef4064282b9
Your Name <you@example.com>
2023-12-26 13:49:55 -0300

    Modificacion en el componente QR

---
+++ src/components/ImagenQR.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const ImagenQR = () => {
+  return (
+    <div
+      className="w-auto h-[345px] rounded-[15px] bg-contain 
+      bg-[url(images/image-qr-code.png)]
+      "
+    ></div>
+  );
+};
+export default ImagenQR;
--- src/components/QrComponent.jsx
+++ src/components/QrComponent.jsx
@@ -1,14 +1,13 @@
 import React from "react";
+import ImagenQR from "./ImagenQR.JSX";
 
 export const QrComponent = () => {
   return (
     <><div className="bg-light-gray bg-auto  h-screen flex justify-center items-center">
       <div className="max-w-96 rounded-2xl bg-white overflow-hidden shadow-lg text-center">
-        <img
-          className="rounded-[35px] p-5"
-          src="./images/image-qr-code.png"
-          alt="QR Code for Frontend Mentor"
-        />
+      <div className="p-5">
+            <ImagenQR />
+          </div>
         <div className="px-6 py-4">
           <div className="font-bold text-2xl mb-5 px-2 ">
             Improve your front-end skills by building projects
